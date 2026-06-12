3963da7889cf349eaf687b6866f7175316bfad1b
newbie-crypt <aliabohammar2@gmail.com>
2026-06-12 23:00:10 +0300

    First commit

---
+++ main.py
@@ -0,0 +1,29 @@
+def main():
+    # Process the command line arguments using the built in parser
+    # Use a match statement to identify the user's action
+    # Execute the action
+
+
+def init():
+    ...
+
+def add():
+    ...
+
+def reset():
+    ...
+
+def commit():
+    ...
+
+def restore():
+    ...
+
+def log():
+    ...
+
+def status():
+    ...
+
+if __name__ == "__main__":
+    main()
