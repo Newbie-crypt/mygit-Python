0c06139283d3d6043b76401e78c59a8a0c17ea4a
newbie-crypt <aliabohammar2@gmail.com>
2026-06-12 23:51:57 +0300

    Add the match statement to main.py to identify command-line arguments, and make requirements.txt

--- main.py
+++ main.py
@@ -1,22 +1,41 @@
+import argparse
+
 def main():
     # Process the command line arguments using the built in parser
     # Use a match statement to identify the user's action
     # Execute the action
+    parser = argparse.ArgumentParser(description="A simple reimplementation of git!")
+    parser.add_argument("command", type=str, help="commands include init, add, commit, reset and so on!")
+    args = parser.parse_args()
+    match args.command:
+        case "init":
+            init()
+        case "add":
+            # Figure out how to identify the file_path and/or filenames
+            ...
+        case "reset":
+            # (the reverse of add)
+            ...
+        case "commit":
+            # Call Commit with a message
+            ...
+        case "log":
+            log()
+        case "status":
+            status()
+
 
 
 def init():
     ...
 
-def add():
+def add(file_path):
     ...
 
 def reset():
     ...
 
-def commit():
-    ...
-
-def restore():
+def commit(message):
     ...
 
 def log():
